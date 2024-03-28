b2ef6c23c4c6e2838226fccb37e3c9c49f0e7c38
manishdoley23 <manishdoley23@gmail.com>
2024-03-28 17:24:45 +0530

    add jwt

--- src/pages/auth/login/Login.tsx
+++ src/pages/auth/login/Login.tsx
@@ -26,23 +26,30 @@ const Login = () => {
 					headers: {
 						"Content-type": "application/json",
 					},
+					credentials: "include",
 					body: JSON.stringify(formData),
 				}
 			);
 
 			console.log("response:", response);
 			const { message } = await response.json();
-			if (response.ok) {
-				console.log("Success");
-				toast(message);
-				login("logged in");
-				setTimeout(() => {
-					navigate("/time");
-				}, 5000);
-			} else {
-				toast(message);
-				console.error("Error in response");
-			}
+			console.log("message:", message);
+			const resHeaders = response.headers;
+			console.log("resHeaders:", resHeaders);
+			const resHeaderCookie = response.headers.get("Set-Cookie");
+			console.log("resHeaderCookie:", resHeaderCookie);
+
+			// if (response.ok) {
+			// 	console.log("Success");
+			// 	toast(message);
+			// 	login("logged in");
+			// 	setTimeout(() => {
+			// 		navigate("/time");
+			// 	}, 5000);
+			// } else {
+			// 	toast(message);
+			// 	console.error("Error in response");
+			// }
 		} catch (error) {
 			console.log("error in signup:", error);
 			throw error;
@@ -50,7 +57,7 @@ const Login = () => {
 	};
 
 	return (
-		<div className="h-screen w-screen flex items-center justify-center">
+		<div className="h-screen w-screen flex flex-col items-center justify-center">
 			<ToastContainer
 				position="bottom-center"
 				autoClose={5000}
@@ -64,7 +71,7 @@ const Login = () => {
 				theme="dark"
 				transition={Bounce}
 			/>
-			<form onSubmit={onSubmitHandler}>
+			<form className="flex flex-col" onSubmit={onSubmitHandler}>
 				<header>Login page</header>
 				<label>Email</label>
 				<input
@@ -92,6 +99,18 @@ const Login = () => {
 
 				<button>SUBMIT</button>
 			</form>
+			<button
+				onClick={async () => {
+					await fetch(`${import.meta.env.VITE_BACKEND}/check`, {
+						headers: {
+							"Content-type": "application/json",
+							Authorization: "Bearer tukka",
+						},
+					});
+				}}
+			>
+				TEST
+			</button>
 		</div>
 	);
 };
