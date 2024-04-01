4e27c6e357e71bc372cf7d0a8f6bee5ba9568a35
manishdoley23 <manishdoley23@gmail.com>
2024-04-01 15:45:00 +0530

    add logout handler

--- src/pages/welcome/Welcome.tsx
+++ src/pages/welcome/Welcome.tsx
@@ -1,5 +1,30 @@
+import { useAuth } from "../../hooks/auth.hook";
+
 const Welcome = () => {
-	return <div>Welcome page</div>;
+	const { setUser } = useAuth();
+
+	const logoutHandler = async () => {
+		await fetch(`${import.meta.env.VITE_BACKEND}/auth/logout`, {
+			credentials: "include",
+		});
+		setUser({
+			accessToken: "",
+			email: "",
+			password: "",
+		});
+	};
+
+	return (
+		<>
+			<div>Welcome page</div>
+			<button
+				className="border border-black px-3 py-2"
+				onClick={logoutHandler}
+			>
+				Log out
+			</button>
+		</>
+	);
 };
 
 export default Welcome;
